ffb12a7a5bfd6b64a48b890cd89aaa4e7b9bdeba
Peggy <papax1123@gmail.com>
2024-01-22 14:37:04 +0800

    git add

---
+++ src/views/AboutView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <div>{{ data }}</div>
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+
+</style>
+
+<script>
+export default {
+  data(){
+    return {
+      data:{}
+    }
+  },
+  mounted(){
+    console.log(import.meta.env.VITE_TEXT);
+    
+    const url = `import.meta.env.VITE_PATH` //改為環境變數
+    this.$http.get(url)
+    .then(res=>{
+      console.log(res);
+      this.data = res.data
+    })
+  }
+}
+</script>
